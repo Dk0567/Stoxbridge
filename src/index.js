1d35ddbd177a99fc5b6c5f8de86479e344ad9652
Deepak Prajapati <parjapatideepak2018@gmail.com>
2026-01-21 14:17:56 +0530

    fix case-sensitive folder issue

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
-import ProductPage from "./landing_page/product/ProductPage.js";
+import ProductPage from "./landing_page/Product/ProductPage.js";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import Supportpage from "./landing_page/support/SupportPage";
 import Navbar from "./landing_page/Navbar.js";
